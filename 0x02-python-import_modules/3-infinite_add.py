5e82433918fe67968b735b0a0b5a251ea39026bb
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-05-11 22:14:14 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        sum = 0
-        while i <= n:
-            sum += int(argv[i])
-            i += 1
-        print("{:d}".format(sum))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
 
 if __name__ == "__main__":
     import sys
