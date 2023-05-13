baf47a831cff4675ecc05b096114b4018a30f2da
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-05-13 09:37:50 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
+ * is_palindrome - check if listint_t linked list is a palindrome
+ * @head: a pointer to a pointer to a struct
  *
- * Return: A pointer to the head of the reversed list.
+ * Return: 1 if palindrome, else 0
  */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
-
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
- */
+listint_t *reverse_list(listint_t **head);
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
+	listint_t *slow = *head, *fast = *head, *tmp = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (!*head)
 		return (1);
-
-	tmp = *head;
-	while (tmp)
+	while (fast->next && fast->next->next)
 	{
-		size++;
-		tmp = tmp->next;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
+	slow = reverse_list(&slow);
+	tmp = slow;
+	fast = *head;
+	while (slow && fast)
 	{
-		if (tmp->n != rev->n)
+		if (slow->n != fast->n)
 			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
+		slow = slow->next;
+		fast = fast->next;
 	}
-	reverse_listint(&mid);
-
+	reverse_list(&tmp);
 	return (1);
+}
+/**
+ * reverse_list - reverses a listint_t linked list
+ * @head: a pointer to a pointer to a struct
+ *
+ * Return: a pointer to first node of reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *ahead = NULL;
+
+	while (*head)
+	{
+		ahead = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = ahead;
+	}
+	*head = prev;
+	return (*head);
 }
