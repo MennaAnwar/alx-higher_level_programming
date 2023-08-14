27fb82eecfd80fa76379665b10bdad25b440ebad
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-08-14 14:45:37 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+let num = process.argv[2];
+
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
 }
-
