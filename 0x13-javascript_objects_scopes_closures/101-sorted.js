0ec64b415edeecfe2d1439239589f906306593e5
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-08-15 18:26:59 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 
-const { dict } = require('./101-data.js');
-const Dictn = {};
-for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [];
-    }
-    Dictn[dict[N]].push(N);
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+const keys = Object.values(dict).map(String);
+const values = Object.keys(dict).map(String);
+
+for (let i = 0; i < values.length; i++) {
+  if (!newDict[keys[i]]) {
+    newDict[keys[i]] = [];
+  }
+
+  newDict[keys[i]].push(values[i]);
 }
-console.log(Dictn);
+
+console.log(newDict);
