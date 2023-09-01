03ebbaa2abb673d02f8f60fb22f233d5885313cb
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-09-01 17:17:20 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,17 +7,12 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    first, last = 0, len(list_of_integers) - 1
+    while first <= last:
+        mid = first + ((last - first) // 2)
+        if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            last = mid - 1
+        elif mid < len(list_of_integers) - 1 and list_of_integers[mid] < list_of_integers[mid + 1]:
+            first = mid + 1
+        else:
+            return list_of_integers[mid]
