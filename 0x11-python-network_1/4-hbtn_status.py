74af0c877cbefdf6962a39060fbf320041adb0ad
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-09-01 15:12:21 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status."""
-import requests
 
 if __name__ == "__main__":
+    import requests
+
     req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
