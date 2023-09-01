5d256dbc31dce76634f0d354fd7224ae8eb0db6b
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-09-01 15:23:56 +0300

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,10 +3,11 @@
 - takes your GitHub credentials (username and password)
 - uses the GitHub API to display your id
 """
+import sys
 import requests
-from sys import argv
-try:
-    req = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(req.json().get('id'))
-except:
-    print("None")
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
